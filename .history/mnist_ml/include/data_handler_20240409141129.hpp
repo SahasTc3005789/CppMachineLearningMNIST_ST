016a38555e6c565828d5df346aad3f2244b89472
Sahas Talasila <sahas.st01@gmail.com>
2024-04-09 14:13:39 +0100

    started adding to data handler class

---
+++ .history/mnist_ml/include/data_handler_20240409141129.hpp
@@ -0,0 +1,22 @@
+/*
+implementing the logic to read, split the data, count the number of unique classes
+and to pass around the test and validation data 
+*/
+
+#ifndef __DATA_HANDLER_H
+#define __DATA_HANDLER_H
+
+#include <fstream>  // to read file.io
+#include "stdint.h" 
+#include "data.hpp" // to read uint8 data type
+#include <vector>
+#include <string> // strings make it easier, don't have to worry about construct pointers
+#include <map> // to map a class label to enum value, quick access
+#include <unordered_set> // keeps track of train data, test data and validation data
+
+class data_handler
+{
+
+};
+
+#endif
